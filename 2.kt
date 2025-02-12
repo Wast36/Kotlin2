26ad86b56c43d376f1f8eac75e7989a39eb0e952
Wast36 <daniil2007kapystnikov@gmail.com>
2025-02-12 11:00:48 +0300

    Update 2.kt

--- 2.kt
+++ 2.kt
@@ -1,25 +1,25 @@
-fun calculateCommission
-    (cardType: String = "VK Pay",
-    previousTransfers: Double = 0.0,
-    transferAmount: Double): Double
+import java.util.*
+
+fun calculateCommission (cardType: String = "VK Pay", previousTransfers: Double = 0.0, transferAmount: Double): Double
 {
-    return when (cardType)
+    return when (cardType.lowercase(Locale.getDefault()))
     {
-        "MasterCard", "Maestro" ->
-            {
+        "mastercard", "maestro" ->
+        {
             if (previousTransfers + transferAmount <= 75000) 0.0
             else (transferAmount * 0.006).coerceAtLeast(20.0)
         }
-        "VK Pay" -> 0.0
-        else  -> (transferAmount * 0.0075).coerceAtLeast(35.0) // Комиссия для карт "Мир" и "Visa"
+        "vk pay" -> 0.0
+        "мир" , "visa" -> (transferAmount * 0.0075).coerceAtLeast(35.0) // Комиссия для карт "Мир" и "Visa"
+        else -> -1.0
 
     }
 }
 
 fun main()
 {
-    val amount = 76000.0
-    val cardType = "Visa"
+    val amount = 86000.0
+    val cardType = "vk pay"
     val previousTransfers = 0.0
 
     val commission = calculateCommission(cardType, previousTransfers, amount)
